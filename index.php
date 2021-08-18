70ef94c56d61a7a2b898db167e990b3649be9144
claudegose <84382636+claudegose@users.noreply.github.com>
2021-08-18 16:01:29 +0200

    Project Completed & Operational!

--- index.php
+++ index.php
@@ -6,11 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
     <title>CRUD</title>
 </head>
 <body>
+
 <h1 align="center" style="color:#2471A3  ;">Dream Location Planner</h1>
 <?php require_once "process.php"; ?>
+
+<?php
+if (isset($_SESSION["message"])): ?>
+
+
+<div class="alert alert-<?=$_SESSION["msg_type"]?> ">
+
+    <?php
+        echo $_SESSION["message"];
+        unset($_SESSION["message"]);
+    ?>
+
+</div>
+
+<?php endif ?>
 <div class="container">
 <?php
 $mysqli = new mysqli("localhost", "root","root","crud") or die(mysqli_error($mysqli));
@@ -53,18 +70,28 @@ $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
     }
     ?>
 
-<div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center">
+<div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center" id="divmain">
 <form action="process.php" method="POST">
-             <div class="form-group text-center ">
+             <div class="form-group text-center" id="haha1">
                 <label>Name</label>
-                <input type="type" name="name" class="form-control" placeholder="Enter your name">
+                <input type="type" name="name" class="form-control"
+                       value="<?php echo $name; ?>" placeholder="Enter your name">
              </div>
-             <div class="form-group text-center ">
+             <div class="form-group text-center" id="haha2">
                 <label>Enter your Dream Location</label>
-                <input type="type2" name="location" class="form-control" placeholder="Enter your Dream location">
+                <input type="type2" name="location" class="form-control"
+                       value="<?php  echo $location; ?>" placeholder="Enter your Dream location">
             </div>
-            <div class="form-group text-center ">
+            <div class="form-group text-center " id="haha3">
+                <?php
+                if ($update == true):
+                ?>
+                 <button type="submit" class="btn btn-info" name="update">Update</button>
+                endif;
+                <?php else: ?>
+
                 <button type="submit" class="btn btn-outline-dark " name="save">Save</button>
+            <?php endif; ?>
             </div>
 </form>
 </div>
--- process.php
+++ process.php
@@ -1,5 +1,11 @@
 <?php
+session_start();
+
 $mysqli = new mysqli("localhost", "root","root","crud") or die(mysqli_error());
+$update = false;
+$name= "";
+$location = "";
+
 
 if (isset($_POST['save']))
 {
@@ -9,9 +15,33 @@ if (isset($_POST['save']))
     $mysqli->query("INSERT INTO data (name, location) VALUES('$name', '$location') ") or
             die($mysqli->error);
 
+    $_SESSION['message'] = "Record has been saved!";
+    $_SESSION['msg_type'] = "Success";
+
+    header("location: index.php");
+
+
 }
 
-if (isset($_GET["delete"])){
+if (isset($_GET["delete"]))
+{
     $id = $_GET["delete"];
-    $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error);
+    $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error());
+
+    $_SESSION['message'] = "Record has been deleted!";
+    $_SESSION['msg_type'] = "Danger";
+
+    header("location: index.php");
+}
+
+
+if (isset($_GET["edit"])){
+    $id = $_GET["edit"];
+    $update = true;
+    $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error());
+    if (count($result)==1){
+        $row = $result->fetch_array();
+        $name = $row["name"];
+        $location = $row["location"];
+    }
 }
