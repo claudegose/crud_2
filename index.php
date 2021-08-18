fe836621828d23067cd58b94bec1b3e78b990d99
claudegose <84382636+claudegose@users.noreply.github.com>
2021-08-18 09:40:43 +0200

    BS Styling Actioned

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 <form action="" method="post">
             <div class="form-group">
                 <label>Name</label>
+
             <input type="type" name="name" class="form-control" value="Enter your name">
              </div>
              <div class="form-group">
